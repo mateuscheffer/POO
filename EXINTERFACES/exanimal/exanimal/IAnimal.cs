3d18e86742624ba76daf3035fd26ae79a27dc946
mateuscheffer <mateus.scheffer@rede.ulbra.br>
2023-04-26 19:52:02 -0300

    exinterfaces

---
+++ EXINTERFACES/exanimal/exanimal/IAnimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public interface IAnimal
+{
+    void EmitirSom();
+}
+
+public class Cachorro : IAnimal
+{
+    public void EmitirSom()
+    {
+        Console.WriteLine("Au au!");
+    }
+}
+
+public class Gato : IAnimal
+{
+    public void EmitirSom()
+    {
+        Console.WriteLine("Miau!");
+    }
+}
+
+public class Papagaio : IAnimal
+{
+    public void EmitirSom()
+    {
+        Console.WriteLine("Olá!");
+    }
+}
---
+++ EXINTERFACES/exanimal/exanimal/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var cachorro = new Cachorro();
+        var gato = new Gato();
+        var papagaio = new Papagaio();
+
+        cachorro.EmitirSom();
+        gato.EmitirSom();
+        papagaio.EmitirSom();
+
+        Console.ReadKey();
+    }
+}
